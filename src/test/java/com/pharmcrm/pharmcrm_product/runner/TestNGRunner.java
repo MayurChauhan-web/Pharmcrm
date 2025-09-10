c2e5f3fb9b64995524fd9d9668d03830ce030dd7
mchauhan9 <mchauhan@valcaresystems.com>
2025-09-10 12:16:33 +0530

    Added test scenarios for Patient Module and Patient Details

--- src/test/java/com/pharmcrm/pharmcrm_product/runner/TestNGRunner.java
+++ src/test/java/com/pharmcrm/pharmcrm_product/runner/TestNGRunner.java
@@ -4,22 +4,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(
-    features = "src/test/resources/Features/UserManagement.feature",
-    glue = "com.pharmcrm.pharmcrm_product.steps",
-    plugin = {
-        "pretty",
-        "html:target/cucumber-report.html",
-        "json:target/cucumber.json"
-    },
-    monochrome = true,
-    tags = "@login"  
-)
+@CucumberOptions(features = "src/test/resources/Features/UserManagement.feature", glue = "com.pharmcrm.pharmcrm_product.steps", plugin = {
+		"pretty", "html:target/cucumber-report.html",
+		"json:target/cucumber.json" }, monochrome = true, tags = "@Drug")
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -12,22 +12,21 @@ public class DriverFactory {
 	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
 	public static void initDriver() {
-		if (driver.get() == null) {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--start-maximized");
-			options.addArguments("--remote-allow-origins=*");
-			Map<String, Object> prefs = new HashMap<>();
-			String appState = "{"
-					+ "\"recentDestinations\":[{\"id\":\"Save as PDF\",\"origin\":\"local\",\"account\":\"\"}],"
-					+ "\"selectedDestinationId\":\"Save as PDF\"," + "\"version\":2" + "}";
-			prefs.put("printing.print_preview_sticky_settings.appState", appState);
-			prefs.put("savefile.default_directory", "C:\\Users\\MayurChauhan\\Downloads");
-			options.setExperimentalOption("prefs", prefs);
-			options.addArguments("--kiosk-printing");
-			WebDriver chromeDriver = new ChromeDriver(options);
-			chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-			driver.set(chromeDriver);
-		}
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		options.addArguments("--remote-allow-origins=*");
+		Map<String, Object> prefs = new HashMap<>();
+		String appState = "{"
+				+ "\"recentDestinations\":[{\"id\":\"Save as PDF\",\"origin\":\"local\",\"account\":\"\"}],"
+				+ "\"selectedDestinationId\":\"Save as PDF\"," + "\"version\":2" + "}";
+		prefs.put("printing.print_preview_sticky_settings.appState", appState);
+		prefs.put("savefile.default_directory", "C:\\Users\\MayurChauhan\\Downloads");
+		options.setExperimentalOption("prefs", prefs);
+		options.addArguments("--kiosk-printing");
+		WebDriver chromeDriver = new ChromeDriver(options);
+		chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.set(chromeDriver);
+
 	}
 
 	public static WebDriver getDriver() {
